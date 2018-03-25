32b63f8b9ab9d5b472de9358ee56cc64c964f463
Patryk <sdark431@gmail.com>
2018-03-25 18:31:46 +0200

    Dodanie obrotow RR, RL, LR

--- AVL.cpp
+++ AVL.cpp
@@ -66,12 +66,74 @@ void CAVL::obrocLL(TWezel * wezel) {
 
 void CAVL::obrocRR(TWezel * wezel) {
     printf("RR\n");
+    TWezel * dziecko = wezel->prawy;
+    wezel->prawy = dziecko->lewy;
+    if ( dziecko->lewy ) {
+        dziecko->lewy->rodzic = wezel;
+    }
+    dziecko->lewy = wezel;
+    dziecko->rodzic = wezel->rodzic;
+    if ( wezel->rodzic ) {
+        if ( dziecko->klucz < wezel->rodzic->klucz ) {
+            wezel->rodzic->lewy = dziecko;
+        } else {
+            wezel->rodzic->prawy = dziecko;
+        }
+    } else {
+        korzen = dziecko;
+    }
+    wezel->rodzic = dziecko;
+    if ( dziecko->balans == WAGA_PLUS_JEDEN ) {
+        wezel->balans = WAGA_ZERO;
+        dziecko->balans = WAGA_ZERO;
+    } else {
+        wezel->balans = WAGA_PLUS_JEDEN;
+        dziecko->balans = WAGA_MINUS_JEDEN;
+    }
 }
 
 void CAVL::obrocLR(TWezel * wezel) {
     printf("LR\n");
+    TWezel * dziecko = wezel->lewy;
+    TWezel * nastepneDziecko = dziecko->prawy;
+    int tmp = wezel->klucz;
+    wezel->klucz = nastepneDziecko->klucz;
+    nastepneDziecko->klucz = tmp;
+    dziecko->prawy = nastepneDziecko->lewy;
+    if ( nastepneDziecko->lewy ) {
+        nastepneDziecko->lewy->rodzic = dziecko;
+    }
+    nastepneDziecko->lewy = nastepneDziecko->prawy;
+    nastepneDziecko->prawy = wezel->prawy;
+    if ( wezel->prawy ) {
+        wezel->prawy->rodzic = nastepneDziecko;
+    }
+    wezel->prawy = nastepneDziecko;
+    nastepneDziecko->rodzic = wezel;
+    wezel->balans = ( nastepneDziecko->balans == WAGA_MINUS_JEDEN ) ? WAGA_PLUS_JEDEN : WAGA_ZERO;
+    dziecko->balans = ( nastepneDziecko->balans == WAGA_PLUS_JEDEN ) ? WAGA_MINUS_JEDEN : WAGA_ZERO;
+    nastepneDziecko->balans = WAGA_ZERO;
 }
 
 void CAVL::obrocRL(TWezel * wezel) {
     printf("RL\n");
+    TWezel * dziecko = wezel->prawy;
+    TWezel * nastepneDziecko = dziecko->lewy;
+    int tmp = wezel->klucz;
+    wezel->klucz = nastepneDziecko->klucz;
+    nastepneDziecko->klucz = tmp;
+    dziecko->lewy = nastepneDziecko->prawy;
+    if ( nastepneDziecko->prawy ) {
+        nastepneDziecko->prawy->rodzic = dziecko;
+    }
+    nastepneDziecko->prawy = nastepneDziecko->lewy;
+    nastepneDziecko->lewy = wezel->lewy;
+    if ( wezel->lewy ) {
+        wezel->lewy->rodzic = nastepneDziecko;
+    }
+    wezel->lewy = nastepneDziecko;
+    nastepneDziecko->rodzic = wezel;
+    wezel->balans = ( nastepneDziecko->balans == WAGA_PLUS_JEDEN ) ? WAGA_MINUS_JEDEN : WAGA_ZERO;
+    dziecko->balans = ( nastepneDziecko->balans == WAGA_MINUS_JEDEN ) ? WAGA_PLUS_JEDEN : WAGA_ZERO;
+    nastepneDziecko->balans = WAGA_ZERO;
 }
--- main.cpp
+++ main.cpp
@@ -8,20 +8,21 @@
 
 #include "AVL.h"
 
-#define ILOSC 10
 #define PRT_DODAJ 0
-#define PRT_USUN 0
+#define PRT_USUN 1
 
 int main(int argc, char *argv[]) {
     CAVL avl;
     int  wysokosc;
     char stop;
-    int ilosc = ILOSC;
-    int dlugosc;
-    int tWartosci[] = { 6,3,2,1 };
+    int ilosc;
+//        int tWartosci[] = { 60,30,20,10 }; //LL
+//        int tWartosci[] = { 60,30,20,10,80,100,50 }; //RR
+//        int tWartosci[] = { 60,30,20,10,100,120,50,80,70 }; //LR
+          int tWartosci[] = { 60,30,20,10,100,120,50,80,70,54,57 }; //LR
 
-    dlugosc = sizeof(tWartosci)/sizeof(int);
-    for (int i = 0 ; i < dlugosc ; ++i) {
+    ilosc = sizeof(tWartosci)/sizeof(int);
+    for (int i = 0 ; i < ilosc ; ++i) {
         if( avl.dodaj(tWartosci[i]) ) {
             if(PRT_DODAJ) printf("Dodanie <%d> przed zbalansowaniem\n", tWartosci[i] );
             if(PRT_DODAJ) avl.print();
@@ -38,22 +39,22 @@ int main(int argc, char *argv[]) {
 
 	wysokosc = avl.wysokosc();
     printf("Wysokosc po dodaniu: %d\n", wysokosc);
-    dlugosc = sizeof(tWartosci)/sizeof(int);
-    for (int i = 0 ; i < dlugosc ; ++i) {
+    ilosc = sizeof(tWartosci)/sizeof(int);
+    for (int i = 0 ; i < ilosc ; ++i) {
         if( avl.usun( tWartosci[i] ) ) {
+            if(PRT_USUN) printf("Usuwanie <%d> przed zbalansowaniem\n", tWartosci[i] );
             if(PRT_USUN) avl.print();
             if(PRT_USUN) avl.zbalansuj();
             if(PRT_USUN) printf("\n");
+            if(PRT_USUN) printf("Usuwanie <%d> po zbalansowaniu\n", tWartosci[i] );
             if(PRT_USUN) avl.print();
             if(PRT_USUN) stop = getchar();
         }
-//        system("cls");
     }
 
 	wysokosc = avl.wysokosc();
     printf("Wysokosc po usunieciu: %d\n", wysokosc);
     avl.print();
 
-//    delete [] tWartosci;
     return 0;
 }
